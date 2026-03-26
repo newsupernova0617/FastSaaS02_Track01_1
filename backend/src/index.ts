658a7dd26b6011dc8662bebfe28115ce25bcebbf
mingun <rlaalsrjswww@yonsei.ac.kr>
2026-03-26 21:56:52 +0900

    Solving conflict

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,7 @@ import transactions from './routes/transactions';
 
 const app = new Hono();
 
-<<<<<<< HEAD
 app.use('*', cors({ origin: ['http://localhost:5173', 'capacitor://localhost', 'https://fastsaas02-track01-1.pages.dev'] }));
-=======
-app.use('*', cors({ origin: ['http://localhost:5173', 'https://fastsaas02-track01-1.pages.dev/', 'capacitor://localhost'] }));
->>>>>>> df7e3ac (add backend diagram)
 app.route('/api/transactions', transactions);
 
 export default app;
